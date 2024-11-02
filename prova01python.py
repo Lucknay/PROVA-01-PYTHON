3fa4fce74beadee6e5d726fa8f1acde69a480383
Lucknay <160678001+Lucknay@users.noreply.github.com>
2024-11-02 19:05:30 -0300

    Add files via upload

---
+++ prova01python.py
@@ -0,0 +1,11 @@
+lista1= ['Laranja', 'Maçã', 'Uva', 'abacaxi', 'Pessego', 'Melancia']
+lista = tuple(lista1)
+
+Laranja, Maçã, Uva, abacaxi, Pessego, Melancia = lista
+
+print('A primeira fruta é:', Laranja)
+print('A segunda fruta é:', Maçã)
+print('A terceira fruta é:', Uva)
+print('A quarta fruta é:', abacaxi)
+print('A quinta fruta é:', Pessego)
+print('A sexta fruta é:', Melancia)
